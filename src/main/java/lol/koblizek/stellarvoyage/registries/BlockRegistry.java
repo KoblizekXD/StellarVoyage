0af34203af1827e8e4ace5d0c18bd9eabf2c6a2b
KoblizekXD <janprokupek@centrum.cz>
2023-08-01 13:37:45 +0200

    Created titanium ore block registration (Waiting for @Motulok to add textures)

--- src/main/java/lol/koblizek/stellarvoyage/registries/BlockRegistry.java
+++ src/main/java/lol/koblizek/stellarvoyage/registries/BlockRegistry.java
@@ -1,10 +1,12 @@
 package lol.koblizek.stellarvoyage.registries;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 
 public class BlockRegistry extends IModRegistry<Block> {
     @Override
     void apply() {
-
+        register(new Block(FabricBlockSettings.of(Material.METAL).strength(3)), "titanium_ore");
     }
 }
