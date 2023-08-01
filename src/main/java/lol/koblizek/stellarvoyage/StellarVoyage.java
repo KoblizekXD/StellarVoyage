a91c88be122971ce55f82c9c78faf3a3a634692e
KoblizekXD <janprokupek@centrum.cz>
2023-08-01 12:48:04 +0200

    Initial commit

---
+++ src/main/java/lol/koblizek/stellarvoyage/StellarVoyage.java
@@ -0,0 +1,13 @@
+package lol.koblizek.stellarvoyage;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StellarVoyage implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/lol/koblizek/stellarvoyage/client/StellarVoyageClient.java
@@ -0,0 +1,13 @@
+package lol.koblizek.stellarvoyage.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StellarVoyageClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
