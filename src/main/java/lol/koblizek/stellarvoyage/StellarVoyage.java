a5997d2bf2e0266ab7e8a7816fee24a5549ba8ad
KoblizekXD <janprokupek@centrum.cz>
2023-08-01 13:45:48 +0200

    logger

--- src/main/java/lol/koblizek/stellarvoyage/StellarVoyage.java
+++ src/main/java/lol/koblizek/stellarvoyage/StellarVoyage.java
@@ -4,14 +4,22 @@ import lol.koblizek.stellarvoyage.registries.BlockRegistry;
 import lol.koblizek.stellarvoyage.registries.ItemRegistry;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StellarVoyage implements ModInitializer {
+    /**
+     * Main logger used for mod logging
+     */
+    public static final Logger LOGGER = LoggerFactory.getLogger("Stellar Voyage");
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
+        LOGGER.info("==Begin mod initialization==");
         new BlockRegistry().create(Registry.BLOCK);
         new ItemRegistry().create(Registry.ITEM);
+        LOGGER.info("==End mod initialization==");
     }
 }
